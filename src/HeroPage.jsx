b3cfce8d7e4a7452e39a34cfeb30b31c4534675d
DanielCabador <dcabador8@gmail.com>
2023-06-05 09:59:15 +0800

    added landingpage content with picture, can change

--- src/HeroPage.jsx
+++ src/HeroPage.jsx
@@ -1,9 +1,28 @@
 const HeroPage = () => {
   return (
-    <div>
-      <h1 className="text-heropage mt-5 p-5">This is Hero Page</h1>
+    <div className="hero-page bg-black">
+      <br />
+      <br />
+      <div className="container-fluid my-5">
+        <div className="row">
+          <div className="col-lg-6">
+            <a href="#" className="picture-link">
+              <img src="public\Greenblack-image.jpg" alt="Picture" className="picture-img img-fluid rounded-circle" />
+            </a>
+          </div>
+          <div className="col-lg-6">
+            <div className="text-container text-white">
+              <h1>This is the Home Page</h1>
+              <p>Welcome to our website. Explore and discover amazing content.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default HeroPage;
+
+
+
