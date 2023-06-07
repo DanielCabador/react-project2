1608e6b11e31d2f4cd27dabec910e91599f235a8
jankendall <jk.kuz20@gmail.com>
2023-06-07 15:45:47 +0800

    added text colors

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -10,16 +10,16 @@ const About = () => {
           <div className="row">
             <div className="col-md-12">
               <div className="card text-white ">
-                <div className="card-body bg-black">
+                <div className="card-body bg-black p-5">
                   <div>
-                    <p>
-                      Welcome to our stock market website. We provide up-to-date information and analysis on various stocks and financial markets.
-                    </p>
+                    <h4>
+                      Welcome to <strong className='text-success'>Resolute</strong>! We provide up-to-date information and analysis on various stocks and financial markets.
+                    </h4>
                   </div>
                   <div>
-                    <p>
+                    <h>
                       Our mission is to empower investors with the knowledge and tools they need to make informed decisions in the dynamic world of investing.
-                    </p>
+                    </h>
                   </div>
                 </div>
               </div>
@@ -30,56 +30,38 @@ const About = () => {
               <div className="about-container">
                 <h4 className="text-center text-success m-4">Our Goals</h4>
                 <div className="row">
-                  <div className="col-md-2">
+                  <div className="col-md-3">
                     <div className="card text-white mb-3 h-100">
                       <div className="card-body bg-black">
                         <p className="card-text">
-                          Provide accurate and timely stock market information
+                          <strong className='text-success'>Provide</strong> <br /> accurate and timely stock market information
                         </p>
                       </div>
                     </div>
                   </div>
-                  <div className="col-md-2">
+                  <div className="col-md-3">
                     <div className="card text-white mb-3 h-100">
                       <div className="card-body bg-black">
                         <p className="card-text">
-                          Offer comprehensive analysis of financial markets
+                        <strong className='text-success'>Offer</strong> <br /> comprehensive analysis of financial markets
                         </p>
                       </div>
                     </div>
                   </div>
-                  <div className="col-md-2">
+                  <div className="col-md-3">
                     <div className="card text-white mb-3 h-100">
                       <div className="card-body bg-black">
                         <p className="card-text">
-                          Empower investors with knowledge and tools for informed decision-making
+                        <strong className='text-success'>Empower</strong> <br />investors with knowledge and tools for informed decision-making
                         </p>
                       </div>
                     </div>
                   </div>
-                  <div className="col-md-2">
+                  <div className="col-md-3">
                     <div className="card text-white mb-3 h-100">
                       <div className="card-body bg-black">
                         <p className="card-text">
-                          Facilitate access to diverse investment opportunities
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-2">
-                    <div className="card text-white mb-3 h-100">
-                      <div className="card-body bg-black">
-                        <p className="card-text">
-                          Promote financial literacy and education
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-2">
-                    <div className="card text-white mb-3 h-100">
-                      <div className="card-body bg-black">
-                        <p className="card-text">
-                          Foster collaboration and knowledge sharing within the investment community
+                        <strong className='text-success'>Facilitate</strong> <br /> access to diverse investment opportunities
                         </p>
                       </div>
                     </div>
